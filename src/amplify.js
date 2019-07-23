abe9a9b1eaca86c7cb0eeb1ae31606e56c97386e
Cole Marsteller <cgm32547@gmail.com>
2019-07-22 22:17:29 -0700

    add amplify config file

---
+++ src/amplify.js
@@ -0,0 +1,31 @@
+import Amplify, { Auth } from "aws-amplify";
+import config from "./config";
+
+export default async function configureAmplify() {
+
+  Amplify.configure({
+    Auth: {
+      mandatorySignIn: false,
+      region: config.cognito.REGION,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    },
+    Storage: {
+      region: config.s3.REGION,
+      bucket: config.s3.BUCKET,
+      identityPoolId: config.cognito.IDENTITY_POOL_ID
+    },
+    API: {
+      endpoints: [
+        {
+          name: "stage",
+          endpoint: config.apiGateway.URL,
+          region: config.apiGateway.REGION
+        },
+      ]
+    }
+  });
+
+  const anonymousUser = await Auth.currentCredentials();
+
+  console.log(anonymousUser)
+}
