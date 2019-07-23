5eae98cb21af54a7d243e8c99f96315a407c19d1
Cole Marsteller <cgm32547@gmail.com>
2019-07-22 22:17:03 -0700

    set up react redux with routing

--- src/index.js
+++ src/index.js
@@ -3,12 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { BrowserRouter as Router} from "react-router-dom";
+import { ConnectedRouter } from 'connected-react-router'
+import { Provider } from 'react-redux';
+import configureStore, { history } from "./store/configureStore"
+import configureAmplify from "./amplify"
+
+const store = configureStore();
+
+configureAmplify();
+
+// async function test2() {
+
+//   Amplify.configure({
+//     Auth: {
+//       mandatorySignIn: false,
+//       region: config.cognito.REGION,
+//       identityPoolId: config.cognito.IDENTITY_POOL_ID,
+//     },
+//     Storage: {
+//       region: config.s3.REGION,
+//       bucket: config.s3.BUCKET,
+//       identityPoolId: config.cognito.IDENTITY_POOL_ID
+//     },
+//     API: {
+//       endpoints: [
+//         {
+//           name: "stage",
+//           endpoint: config.apiGateway.URL,
+//           region: config.apiGateway.REGION
+//         },
+//       ]
+//     }
+//   });
+
+//   const anonymousUser = await Auth.currentCredentials();
+
+//   console.log(anonymousUser)
+// }
+
+// test2();
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store = {store}>
+    <ConnectedRouter history={history}>
+        <App />
+    </ConnectedRouter>
+  </Provider>,
   document.getElementById("root")
 );
 
