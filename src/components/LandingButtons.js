231149166aff8bf9b76b781bd7cbeb4705578f9f
Cole Marsteller <cgm32547@gmail.com>
2019-07-23 01:01:55 -0700

    clean up code

--- src/components/LandingButtons.js
+++ src/components/LandingButtons.js
@@ -1,4 +1,4 @@
-import React, { Component }  from "react";
+import React, { Component } from "react";
 import { Redirect } from 'react-router-dom';
 import { Button } from 'semantic-ui-react'
 import { Grid } from 'semantic-ui-react'
@@ -7,22 +7,22 @@ import "./LandingButtons.css";
 export default class LandingButtons extends Component {
   constructor(props) {
     super(props);
-    this.state = {navigateTo: ""};
+    this.state = { navigateTo: "" };
 
     this.routeChange = this.routeChange.bind(this);
   }
 
 
   routeChange(e) {
-    this.setState({navigateTo: `/${e.target.textContent.toLowerCase()}`})
+    this.setState({ navigateTo: `/${e.target.textContent.toLowerCase()}` })
   }
-  
+
   render() {
     if (this.state.navigateTo.length > 0) {
       return <Redirect push to={this.state.navigateTo} />
     }
-    return(
-    <Grid className="landingButtons" textAlign='center' columns={8}>
+    return (
+      <Grid className="landingButtons" textAlign='center' columns={8}>
         <Grid.Row>
           <Grid.Column>
             <Button fluid onClick={this.routeChange}>Buyer</Button>
@@ -31,9 +31,8 @@ export default class LandingButtons extends Component {
             <Button fluid onClick={this.routeChange}>Seller</Button>
           </Grid.Column>
         </Grid.Row>
-        <span className="link-block">Or <a href="123" style={{color:"inherit"}}>Log in</a></span>
+        <span className="link-block">Or <a href="123" style={{ color: "inherit" }}>Log in</a></span>
       </Grid>
     )
   }
 }
-
