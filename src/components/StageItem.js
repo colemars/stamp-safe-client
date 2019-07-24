6a3a52284ebe5567ea34579106f08b76c9c818ca
Cole Marsteller <cgm32547@gmail.com>
2019-07-24 15:16:32 -0700

    make functional component, connect to redux, and disconnect from api

--- src/components/StageItem.js
+++ src/components/StageItem.js
@@ -1,166 +1,175 @@
-import React, { Component }  from "react";
+import React, { useState } from "react";
 import { Grid, Input, Header, Button } from 'semantic-ui-react'
 import { Redirect } from 'react-router-dom';
 import "./StageItem.css";
 import { API } from "aws-amplify";
+import { connect } from 'react-redux';
+import { addFields } from '../actions/index'
 
 const disabledButtonStyle = {
   backgroundColor: "#E6E6E6",
   cursor: "default"
 }
 
+const StageItem = (props) => {
 
-export default class StageItem extends Component  {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isLoading: null,
-      serialNumber: "",
-      make: "",
-      model: "",
-      yearManufactored: "",
-      condition: "",
-      previousOwners: "",
-      price: "",
-      navigateTo: ""
-    };
+  const [loading, setLoading] = useState(null);
+  const [serialNumber, setSerialNumber] = useState(null);
+  const [make, setMake] = useState(null);
+  const [model, setModel] = useState(null);
+  const [yearManufactored, setYearManufactored] = useState(null);
+  const [condition, setCondition] = useState(null);
+  const [previousOwners, setPreviousOwners] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [route, setRoute] = useState(null);
 
-    this.routeChange = this.routeChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.validateForm = this.validateForm.bind(this);
+  const validateForm = () => {
+    return serialNumber > 0 &&
+      make > 0 &&
+      model > 0 &&
+      yearManufactored > 0 &&
+      condition > 0 &&
+      previousOwners > 0 &&
+      price > 0;
   }
 
-  validateForm() {
-    return this.state.serialNumber.length > 0 &&
-      this.state.make.length > 0 &&
-      this.state.model.length > 0 &&
-      this.state.yearManufactored.length > 0 &&
-      this.state.condition.length > 0 &&
-      this.state.previousOwners.length > 0 &&
-      this.state.price.length > 0;
-  }
-
-  stageUpload(fields) {
+  const stageUpload = fields => {
     return API.post("stage", "/stage", {
       body: fields
     });
   }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.id]: event.target.value
-    });
-    console.log("change")
+  const routeChange = () => {
+    setRoute("/upload")
   }
 
-  routeChange() {
-    this.setState({navigateTo: `/upload`})
+  const handleSubmit = async () => {
+    const fields = [
+      serialNumber,
+      make,
+      model,
+      yearManufactored,
+      condition,
+      previousOwners,
+      price
+    ]
+
+    props.addFields(fields)
+
+    routeChange()
+
+    // setLoading(true);
+
+    // try {
+    //   await stageUpload({
+    //     serialNumber: serialNumber,
+    //     make: make,
+    //     model: model,
+    //     yearManufactored: yearManufactored,
+    //     conditionOfItem: condition,
+    //     previousOwners: previousOwners,
+    //     price: price
+    //   });
+    //   routeChange();
+    //   setLoading(false);
+    // } catch (e) {
+    //   alert(e);
+    //   setLoading(false);
+    // }
+  }
+  if (route) {
+    return <Redirect push to='/upload' />
   }
 
-  handleSubmit = async event => {
-    this.setState({ isLoading: true });
-
-    try {
-      await this.stageUpload({
-        serialNumber: this.state.serialNumber,
-        make: this.state.make,
-        model: this.state.model,
-        yearManufactored: this.state.yearManufactored,
-        conditionOfItem: this.state.condition,
-        previousOwners: this.state.previousOwners,
-        price: this.state.price
-      });
-      this.routeChange();
-    } catch (e) {
-      alert(e);
-      this.setState({ isLoading: false });
-    }
-  }
-  
-  render() {
-    if (this.state.navigateTo.length > 0) {
-      return <Redirect push to='/upload' />
-    }
-    let test = this.validateForm() 
-    ? <Button fluid onClick={this.handleSubmit} style={{backgroundColor: "#313131"}} content='Submit' />
+  let button = validateForm()
+    ? <Button fluid onClick={handleSubmit} style={{ backgroundColor: "#313131" }} content='Submit' />
     : <Button fluid style={disabledButtonStyle} content='Submit' />
-    return (
-      <div className="stageItem">
-        <Grid textAlign='center' columns={2}>
-          <Grid.Row>
-            <Grid.Column>
-              <Header as='h1'>Stage Item</Header>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row className="stageItemFooterRow">
-            <Grid.Column>
-              <span id="stageItemFooter">Looking for an item you've already staged? <a href="123" style={{ color: "inherit" }}>Find it here</a></span>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="serialNumber" className="itemInput" onChange={this.handleChange}
-              value={this.state.serialNumber || ''} transparent placeholder='Serial Number' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="make" className="itemInput" onChange={this.handleChange}
-              value={this.state.make || ''} transparent placeholder='Make' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="model" className="itemInput" onChange={this.handleChange}
-              value={this.state.model || ''} transparent placeholder='Model' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="yearManufactored" className="itemInput" onChange={this.handleChange}
-              value={this.state.yearManufactored || ''} transparent placeholder='Year Manufactored' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="condition" className="itemInput" onChange={this.handleChange}
-              value={this.state.condition || ''} transparent placeholder='Condition' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="previousOwners" className="itemInput" onChange={this.handleChange}
-              value={this.state.previousOwners || ''} transparent placeholder='Previous Owners' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              <div className="itemInputBox">
-                <Input id="price" className="itemInput" onChange={this.handleChange}
-              value={this.state.price || ''} transparent placeholder='Price' />
-              </div>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
-            <Grid.Column>
-              {test}
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </div>
-    )
-  }
+    
+  return (
+    <div className="stageItem">
+      <Grid textAlign='center' columns={2}>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as='h1'>Stage Item</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row className="stageItemFooterRow">
+          <Grid.Column>
+            <span id="stageItemFooter">Looking for an item you've already staged? <a href="123" style={{ color: "inherit" }}>Find it here</a></span>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="serialNumber" className="itemInput" onChange={e => setSerialNumber(e.target.value)}
+                transparent placeholder='Serial Number' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="make" className="itemInput" onChange={e => setMake(e.target.value)}
+                transparent placeholder='Make' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="model" className="itemInput" onChange={e => setModel(e.target.value)}
+                transparent placeholder='Model' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="yearManufactored" className="itemInput" onChange={e => setYearManufactored(e.target.value)}
+                transparent placeholder='Year Manufactored' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="condition" className="itemInput" onChange={e => setCondition(e.target.value)}
+                transparent placeholder='Condition' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="previousOwners" className="itemInput" onChange={e => setPreviousOwners(e.target.value)}
+                transparent placeholder='Previous Owners' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input id="price" className="itemInput" onChange={e => setPrice(e.target.value)}
+                transparent placeholder='Price' />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            {button}
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  )
 }
+
+const mapStateToProps = state => ({
+  fields: state.fields
+})
+
+const mapDispatchToProps = dispatch => ({
+  addFields: fields => dispatch(addFields(fields))
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(StageItem)
