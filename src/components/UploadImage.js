c9e9fe8e55c09d623ff8f43a340a14212208d9bf
Cole Marsteller <cgm32547@gmail.com>
2019-07-23 18:23:14 -0700

    make float image preview its own component

--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -1,86 +1,27 @@
-import React, { useState } from "react";
-import { Grid, Input, Header, Button, Divider, Icon } from 'semantic-ui-react'
+import React from "react";
+import { Grid, Header, Button, Divider, Icon } from 'semantic-ui-react'
 import "./UploadImage.css";
 import DragNDrop from './fileDragNDropUpload';
-import config from '../config'
-import convertBlob from '../helpers/blobToFile';
-import { v4 } from 'uuid'
-import { connect } from 'react-redux';
-import { addImage } from '../actions/index'
+import PasteImageInput from './PasteImageInput'
+import PreviewFloat from './PreviewFloat'
 
-const style = {
-  backgroundColor: "rgb(50, 50, 50)",
-  color: "white",
-  fontSize: "14px",
-  width: "18vw",
-  borderRadius: "6px"
-}
-
-function UploadImage(props) {
-
-  const [imageUrl, setImageUrl] = useState(
-    ''
-  );
-
-  const [isUrlLoading, setUrlLoading] = useState(
-    false
-  );
-
-  const handleURLPaste = async (e) => {
-    const clipboardData = e.clipboardData || window.clipboardData;
-    const urlString = clipboardData.getData('Text');
-    setUrlLoading(true);
-    setImageUrl(urlString);
-    fetch(urlString)
-      .then(res => res.blob())
-      .then(blob => {
-        console.log(blob)
-        if (config.file.allowedFileSize < blob.size) {
-          console.log("Sorry, URL failed to upload.", "Error: size limit exceeded");
-          setUrlLoading(false);
-          setImageUrl("");
-          return;
-        }
-        if (config.file.allowedFileTypes.indexOf(blob.type) === -1) {
-          console.log("Sorry, URL failed to upload.", "Error: wrong file type");
-          setUrlLoading(false);
-          setImageUrl("");
-          return;
-        }
-        console.log("success");
-        setUrlLoading(false);
-        setImageUrl("");
-        const imageFile = convertBlob(blob, v4());
-        props.addImage(imageFile)
-
-        // creates image to diplay
-        // let objectURL = URL.createObjectURL(convertBlob(blob, v4()));
-        // let myImage = new Image();
-        // myImage.src = objectURL;
-        // console.log(myImage)
-      })
-      .catch(error => {
-        console.log(error)
-        setUrlLoading(false);
-        setImageUrl("");
-      })
-  }
-
-  return (
-    <div className="uploadImage">
-      <Grid textAlign='center' columns={2}>
-        <Grid.Row>
-          <Grid.Column>
-            <Header as='h1'>Image Upload</Header>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row className="uploadImageFooterRow">
-          <Grid.Column>
-            <span id="uploadImageFooter">Choose images with good lighting and detail</span>
-          </Grid.Column>
-        </Grid.Row>
-        <Grid.Row className="uploadImageFooterRow">
-          <Grid.Column>
+const UploadImage = (props) =>
+  <div className="uploadImage">
+    <Grid textAlign='center' columns={2}>
+      <Grid.Row>
+        <Grid.Column>
+          <Header as='h1'>Image Upload</Header>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row className="uploadImageFooterRow">
+        <Grid.Column>
+          <span id="uploadImageFooter">Choose images with good lighting and detail</span>
+        </Grid.Column>
+      </Grid.Row>
+      <Grid.Row className="uploadImageFooterRow">
+        <Grid.Column>
+          <div id="ImageUploadDialog">
+            <PreviewFloat />
             <div id="imageUploadWrapper">
               <DragNDrop />
               <div id="chooseImageBox">
@@ -94,31 +35,16 @@ function UploadImage(props) {
                     &nbsp;
                     &nbsp;
                     &nbsp;
-                </Button>
+                  </Button>
                   <Divider horizontal id="test">Or</Divider>
-                  <Input
-                    loading={isUrlLoading}
-                    onPaste={handleURLPaste}
-                    value={imageUrl}
-                    placeholder='Paste image or URL'
-                    input={<input style={style} />}
-                  />
+                  <PasteImageInput />
                 </div>
               </div>
             </div>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </div>
-  )
-}
+          </div>
+        </Grid.Column>
+      </Grid.Row>
+    </Grid>
+  </div>
 
-const mapStateToProps = state => ({
-  images: state.images
-})
-
-const mapDispatchToProps = dispatch => ({
-  addImage: image => dispatch(addImage(image))
-})
-
-export default connect(mapStateToProps, mapDispatchToProps)(UploadImage)
+export default UploadImage
