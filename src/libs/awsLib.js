91cc81a5dff211d29a14ef7a10f3180467a4ba9f
Cole Marsteller <cgm32547@gmail.com>
2019-07-25 02:08:45 -0700

    create s3 upload function

---
+++ src/libs/awsLib.js
@@ -0,0 +1,19 @@
+import { Storage } from "aws-amplify";
+import v4 from "uuid";
+
+export async function s3Upload(files, accessToken) {
+  const customPrefix = {
+    public: `images/${accessToken}`,
+  };
+  const keys = [];
+  for (const file of files) {
+    const filename = `${v4()}-${file.name}`;
+
+    const stored = await Storage.put(filename, file, {
+      customPrefix: customPrefix
+    });
+
+    keys.push(stored.key);
+  }
+  return keys;
+}
