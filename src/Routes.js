4a646b2623757f9aa9a626d180d1a65abc73b3d5
Cole Marsteller <cgm32547@gmail.com>
2019-07-25 01:59:39 -0700

    add reviewItemStaging to routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Landing from "./components/Landing";
 import Buyer from "./components/Buyer";
 import Seller from "./components/Seller";
 import UploadImage from "./components/UploadImage";
+import Review from "./components/ReviewItemStaging";
 
 export default () =>
   <Switch>
@@ -11,4 +12,5 @@ export default () =>
     <Route path="/buyer" exact component={Buyer} />
     <Route path="/stage" exact component={Seller} />
     <Route path="/upload" exact component={UploadImage} />
+    <Route path="/review" exact component={Review} />
   </Switch>;
