255c6468026acd6ac7b9af75136f201591dbe2df
Cole Marsteller <cgm32547@gmail.com>
2019-07-29 00:27:45 -0700

    format

--- src/libs/awsLib.js
+++ src/libs/awsLib.js
@@ -8,11 +8,9 @@ export async function s3Upload(files, accessToken) {
   const keys = [];
   for (const file of files) {
     const filename = `${v4()}-${file.name}`;
-
     const stored = await Storage.put(filename, file, {
       customPrefix: customPrefix
     });
-
     keys.push(stored.key);
   }
   return keys;
