35c061fd4da94e22993c6ba53a1756ebe4c6080d
Cole Marsteller <cgm32547@gmail.com>
2019-07-30 15:20:14 -0700

    add component for fetching reports

--- src/Routes.js
+++ src/Routes.js
@@ -1,21 +1,23 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
-import Buyer from './components/Buyer';
 import StageItem from './components/StageItem';
 import UploadImage from './components/UploadImage';
 import Review from './components/ReviewItemStaging';
 import ListingReport from './components/ListingReport';
+import GetReport from './components/GetReport';
+import BuyerReport from './components/BuyerReport';
 
 const Routes = () => {
   return (
     <Switch>
       <Route path="/" exact component={Landing} />
-      <Route path="/buyer" exact component={Buyer} />
+      <Route path="/get-report" exact component={GetReport} />
       <Route path="/stage" exact component={StageItem} />
       <Route path="/upload" exact component={UploadImage} />
       <Route path="/review" exact component={Review} />
       <Route path="/stage-report" exact component={ListingReport} />
+      <Route path="/buyer-report" exact component={BuyerReport} />
     </Switch>
   );
 };
---
+++ src/components/GetReport.js
@@ -0,0 +1,162 @@
+import React, { useState } from 'react';
+import { Grid, Input, Header, Button } from 'semantic-ui-react';
+import { Redirect } from 'react-router-dom';
+import './GetReport.css';
+import { connect } from 'react-redux';
+import { API } from 'aws-amplify';
+import { addFields } from '../actions/index';
+
+const disabledButtonStyle = {
+  backgroundColor: '#E6E6E6',
+  cursor: 'default'
+};
+
+// add functionality so that, if user hits back button or re-navigates to his page AFTER hitting submit, the fields show the infomation already submitted and editing them will edit the already existing state instead of submitting a new state
+
+const GetReport = props => {
+  const [accessKey, setAccessKey] = useState(null);
+  const [secureToken, setSecureToken] = useState(null);
+  const [route, setRoute] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  const validateForm = () => {
+    return (accessKey || secureToken) && !(accessKey && secureToken);
+  };
+
+  const handleAccessKeySubmit = async () => {
+    const report = await API.get('stage', `/report/${accessKey}`);
+    if (report.linkedReportIds) {
+      props.addFields({
+        serialNumber: report.serialNumber,
+        make: report.make,
+        model: report.model,
+        yearManufactored: report.yearManufactored,
+        condition: report.conditionOfItem,
+        previousOwners: report.previousOwners,
+        price: report.price
+      });
+      setRoute('/stage-report');
+    }
+    if (report.reportId) {
+      props.addFields({
+        reportId: report.reportId,
+        stageId: report.stageId,
+        serialNumber: report.serialNumber,
+        make: report.make,
+        model: report.model,
+        yearManufactored: report.yearManufactored,
+        condition: report.conditionOfItem,
+        image: report.image,
+        previousOwners: report.previousOwners,
+        price: report.price,
+        backgroundCheckStatus: report.backgroundCheckStatus,
+        stolenPropertyCheckStatus: report.stolenPropertyCheckStatus,
+        priceAlertStatus: report.priceAlertStatus
+      });
+      setRoute('/buyer-report');
+    }
+  };
+
+  const handleSecureTokenSubmit = async () => {
+    const report = await API.get('stage', `/report/${secureToken}`);
+    console.log(report);
+    // if (report) {
+    //   props.addFields({
+    //     reportId: report.reportId,
+    //     stageId: report.stageId,
+    //     serialNumber: report.serialNumber,
+    //     make: report.make,
+    //     model: report.model,
+    //     yearManufactored: report.yearManufactored,
+    //     condition: report.conditionOfItem,
+    //     image: report.image,
+    //     previousOwners: report.previousOwners,
+    //     price: report.price,
+    //     backgroundCheckStatus: report.backgroundCheckStatus,
+    //     stolenPropertyCheckStatus: report.stolenPropertyCheckStatus,
+    //     priceAlertStatus: report.priceAlertStatus
+    //   });
+    //   setRoute('/buyer-report');
+    // } else console.log('not found');
+  };
+
+  const handleSubmit = () => {
+    setLoading(true);
+    if (accessKey) handleAccessKeySubmit();
+    else if (secureToken) handleSecureTokenSubmit();
+    console.log('secureToken');
+  };
+
+  const button = validateForm() ? (
+    <Button
+      fluid
+      onClick={handleSubmit}
+      style={{ backgroundColor: '#313131' }}
+      content="Submit"
+    />
+  ) : (
+    <Button fluid style={disabledButtonStyle} content="Submit" />
+  );
+
+  if (route) {
+    return <Redirect push to={route} />;
+  }
+
+  return (
+    <div className="stageItem">
+      <Grid textAlign="center" columns={2} style={{ paddingTop: '3em' }}>
+        <Grid.Row>
+          <Grid.Column>
+            <Header as="h1">StampSafe Report</Header>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row className="stageItemFooterRow">
+          <Grid.Column>
+            <div id="stageItemFooter" style={{ width: '50%', margin: 'auto' }}>
+              Input your access key or one-time authorization token to get your
+              report
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input
+                id="accessKey"
+                className="itemInput"
+                onChange={e => setAccessKey(e.target.value)}
+                transparent
+                placeholder="Access key"
+              />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>
+            <div className="itemInputBox">
+              <Input
+                id="secureToken"
+                className="itemInput"
+                onChange={e => setSecureToken(e.target.value)}
+                transparent
+                placeholder="Authorization token"
+              />
+            </div>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column>{button}</Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
+};
+
+const mapDispatchToProps = dispatch => ({
+  addFields: fields => dispatch(addFields(fields))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(GetReport);
