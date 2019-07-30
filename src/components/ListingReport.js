e8813011edc8f4b426999826257317eb05895277
Cole Marsteller <cgm32547@gmail.com>
2019-07-29 19:12:53 -0700

    refactor into self contained components

--- src/components/ListingReport.js
+++ src/components/ListingReport.js
@@ -2,22 +2,20 @@ import React, { useState } from "react";
 import { API } from "aws-amplify";
 import { Grid, Header, Image, Icon, Segment, Button } from 'semantic-ui-react'
 import PropTypes from 'prop-types';
-import shieldIcon from ".././shield-lock.png";
-import safeShield from ".././shield-check.png";
-import closeCircle from ".././close-circle.png";
-import information from ".././information.png";
 import accurateLogo from ".././accurate.jpeg";
-import accountAlert from ".././account-alert-outline.png"
 import safe from ".././safe.png";
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import LinkedReportsSegment from './LinkedReportsSegment';
+import StagingDetailsSegment from './StagingDetailsSegment';
+import DescriptiveLinkSegment from './DescriptiveLinkSegment';
+import ActionsSegment from './ActionsSegment'
 
 const ListingReport = (props) => {
   console.log(props)
   const [listings, setListings] = useState();
-  const accessKey = props.accessKey;
   const test = async () => {
-    const report = await API.get("stage", `/stage/${accessKey}`);
+    const report = await API.get("stage", `/stage/${props.accessKey}`);
     setListings(report);
     console.log(report)
     return report;
@@ -34,294 +32,13 @@ const ListingReport = (props) => {
       <Grid centered columns={2}>
         <Grid.Row stretched>
           <Grid.Column>
-            <Segment.Group style={{ width: "70%", marginLeft: "auto" }}>
-              <Segment>
-                <Grid stackable columns={4}>
-                  <Grid.Column width={12}>
-                    <Header textAlign='left' as='h3' style={{
-                      padding: "1em",
-                      paddingTop: "1.5em",
-                    }}>
-                      <Header.Content>Linked Reports</Header.Content>
-                      <Header.Subheader style={{ paddingTop: "1em" }}>
-                        Ensure a report is
-                    <img src={safeShield} style={{
-                          width: "1em",
-                          height: "1em",
-                          marginLeft: ".3em",
-                          marginBottom: "-.1em"
-                        }} alt="" />
-                        Safe before proceeding
-                </Header.Subheader>
-                    </Header>
-                  </Grid.Column>
-                  <Grid.Column width={4} textAlign="left">
-                    <Image src={shieldIcon} style={{
-                      marginLeft: "-1em",
-                      paddingTop: ".5em",
-                      maxWidth: "6em",
-                      height: "auto"
-                    }} />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment style={{ fontFamily: "Roboto", fontSize: "1.1rem", paddingTop: "1.5em", paddingBottom: "1.5em" }}>
-                <Grid columns={2} textAlign="left">
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      John Smith
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      <img src={closeCircle} style={{
-                        width: "1em",
-                        height: "1em",
-                        marginLeft: ".3em",
-                        marginBottom: "-.15em",
-                        marginRight: ".2em"
-                      }} alt="" />
-                      Not Started
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      Deshaun Clark
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      <img src={safeShield} style={{
-                        width: "1em",
-                        height: "1em",
-                        marginLeft: ".3em",
-                        marginBottom: "-.15em",
-                        marginRight: ".2em"
-                      }} alt="" />
-                      Safe
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      Josh Alexander
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      <Icon loading size='small' name='circle notch' color='teal' style={{
-                        // width: "1em",
-                        // height: "1em",
-                        marginLeft: ".3em",
-                        marginBottom: "-.15em"
-                      }} />
-                      In Progress
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                </Grid>
-              </Segment>
-            </Segment.Group>
-            {/* own component start */}
-            <Segment.Group style={{ width: "70%", marginLeft: "auto" }}>
-              <Segment>
-                <Grid stackable columns={4}>
-                  <Grid.Column width={12}>
-                    <Header textAlign='left' as='h3' style={{
-                      padding: "1em",
-                      paddingTop: "1.5em",
-                    }}>
-                      <Header.Content>Staging Details</Header.Content>
-                      <Header.Subheader style={{ paddingTop: "1em" }}>
-                        Information about your item
-                </Header.Subheader>
-                    </Header>
-                  </Grid.Column>
-                  <Grid.Column width={4} textAlign="left">
-                    <Image src={information} style={{
-                      marginLeft: "-1em",
-                      paddingTop: ".5em",
-                      maxWidth: "6em",
-                      height: "auto"
-                    }} />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment style={{ fontFamily: "Roboto", fontSize: "1.1rem", paddingTop: "1.5em", paddingBottom: "1.5em" }}>
-                <Grid columns={2} textAlign="left">
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      Serial Number
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      XXX-XXX-XXX
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      Year/Make/Model
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      2012 Sig Sauer M11-A1
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      Condition
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      Scuff on left side
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0", paddingTop: ".5em", paddingBottom: ".5em" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      Previous Owners
-                </Grid.Column>
-                    <Grid.Column width={7} textAlign="left" style={{ color: "#5F6368" }}>
-                      0
-                </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                </Grid>
-              </Segment>
-            </Segment.Group>
-            {/* own component end */}
+            <LinkedReportsSegment stageAccessKey={props.accessKey} />
+            <StagingDetailsSegment details={props.fields} />
           </Grid.Column>
           <Grid.Column>
-            {/* own component start */}
-            <Segment.Group style={{ width: "70%", marginRight: "auto" }}>
-              <Segment>
-                <Grid stackable columns={4}>
-                  <Grid.Column width={12}>
-                    <Header textAlign='left' as='h3' style={{
-                      padding: "1em",
-                      paddingTop: "1em",
-                      paddingBottom: ".5em"
-                    }}>
-                      <Header.Content>Criminal Record</Header.Content>
-                      <Header.Subheader style={{ paddingTop: "1em" }}>
-                        Utilizes Accurate Background Checks to run a Federal Criminal Record Search
-                </Header.Subheader>
-                    </Header>
-                  </Grid.Column>
-                  <Grid.Column width={4} textAlign="left">
-                    <Image src={accurateLogo} style={{
-                      marginLeft: "-1em",
-                      paddingTop: ".5em",
-                      maxWidth: "6em",
-                      height: "auto"
-                    }} />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment style={{ fontFamily: "Roboto", fontSize: "1.1rem", paddingTop: "1.5em", paddingBottom: "1em" }}>
-                <Grid columns={2} textAlign="left">
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      <a href="https://www.w3schools.com" style={{ textDecoration: "none" }}>Start Here</a>
-                    </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                </Grid>
-              </Segment>
-            </Segment.Group>
-            {/* own component end */}
-            {/* own component start */}
-            <Segment.Group style={{ width: "70%", marginRight: "auto" }}>
-              <Segment>
-                <Grid stackable columns={4}>
-                  <Grid.Column width={12}>
-                    <Header textAlign='left' as='h3' style={{
-                      padding: "1em",
-                      paddingTop: "1em",
-                      paddingBottom: ".5em"
-                    }}>
-                      <Header.Content>Stolen Property</Header.Content>
-                      <Header.Subheader style={{ paddingTop: "1em" }}>
-                        This is a non-comprehensive search of various stolen property databases
-                </Header.Subheader>
-                    </Header>
-                  </Grid.Column>
-                  <Grid.Column width={4} textAlign="left">
-                    <Image src={safe} style={{
-                      marginLeft: "-1em",
-                      paddingTop: ".5em",
-                      maxWidth: "6em",
-                      height: "auto"
-                    }} />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment style={{ fontFamily: "Roboto", fontSize: "1.1rem", paddingTop: "1.5em", paddingBottom: "1em" }}>
-                <Grid columns={2} textAlign="left">
-                  {/* own component eventually */}
-                  <Grid.Row style={{ padding: "0" }}>
-                    <Grid.Column width={9} style={{ paddingLeft: "2em" }}>
-                      <a href="https://www.w3schools.com" style={{ textDecoration: "none" }}>View Details</a>
-                    </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                </Grid>
-              </Segment>
-            </Segment.Group>
-            {/* own component end */}
-            {/* own component start */}
-            <Segment.Group style={{ width: "70%", marginRight: "auto" }}>
-              <Segment>
-                <Grid stackable columns={4}>
-                  <Grid.Column width={12}>
-                    <Header textAlign='left' as='h3' style={{
-                      padding: "1em",
-                      paddingTop: "1em",
-                      paddingBottom: ".5em"
-                    }}>
-                      <Header.Content>Actions</Header.Content>
-                      <Header.Subheader style={{ paddingTop: "1em" }}>
-                        Delete your listing or Export as a CSV
-                </Header.Subheader>
-                    </Header>
-                  </Grid.Column>
-                  <Grid.Column width={4} textAlign="left">
-                    <Image src={accountAlert} style={{
-                      marginLeft: "-1em",
-                      paddingTop: ".5em",
-                      maxWidth: "6em",
-                      height: "auto"
-                    }} />
-                  </Grid.Column>
-                </Grid>
-              </Segment>
-              <Segment style={{ fontFamily: "Roboto", fontSize: "1.1rem", paddingTop: "1.5em", paddingBottom: "1em" }}>
-                <Grid columns={2} textAlign="left">
-                  {/* own component eventually */}
-                  <Grid.Row centered columns={3} style={{ padding: "0", paddingBottom: ".6em", paddingTop: ".4em"}}>
-                    <Grid.Column>
-                      <Button style={{ width: "100%" }}>
-                        Backup Key
-                       </Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                      <Button color="orange " style={{ width: "100%" }}>
-                        Export CSV
-                       </Button>
-                    </Grid.Column>
-                    <Grid.Column>
-                      <Button color="red" style={{ width: "100%" }}>
-                        Delete
-                       </Button>
-                    </Grid.Column>
-                  </Grid.Row>
-                  {/* end own component  */}
-                </Grid>
-              </Segment>
-            </Segment.Group>
-            {/* own component end */}
+            <DescriptiveLinkSegment header="Criminal Record" subheader="Utilize Accurate Background Checks to run a Federal Criminal Record Search" url="https://www.google.com/" urlText="Start Here" logo={accurateLogo} />
+            <DescriptiveLinkSegment header="Stolen Property" subheader=" This is a non-comprehensive search of various stolen property databases" url="https://www.google.com/" urlText="View Details" logo={safe} />
+           <ActionsSegment />
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -329,12 +46,9 @@ const ListingReport = (props) => {
   )
 }
 
-ListingReport.propTypes = {
-  accessKey: PropTypes.string
-};
-
 const mapStateToProps = state => ({
-  fields: state.fields.fields
+  fields: state.fields.fields,
+  accessKey: state.accessKey.stageAccessKey
 })
 
 export default connect(mapStateToProps)(ListingReport)
