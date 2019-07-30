5b5454e52132cb0f79999e730158533fb57f2b01
Cole Marsteller <cgm32547@gmail.com>
2019-07-29 19:22:15 -0700

    make prettier

--- src/components/ListingReport.js
+++ src/components/ListingReport.js
@@ -1,21 +1,30 @@
 import React from "react";
-import { Grid, Header } from 'semantic-ui-react'
+import { Grid, Header } from "semantic-ui-react";
 import accurateLogo from ".././accurate.jpeg";
 import safe from ".././safe.png";
-import { Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import LinkedReportsSegment from './LinkedReportsSegment';
-import StagingDetailsSegment from './StagingDetailsSegment';
-import DescriptiveLinkSegment from './DescriptiveLinkSegment';
-import ActionsSegment from './ActionsSegment'
+import { Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import LinkedReportsSegment from "./LinkedReportsSegment";
+import StagingDetailsSegment from "./StagingDetailsSegment";
+import DescriptiveLinkSegment from "./DescriptiveLinkSegment";
+import ActionsSegment from "./ActionsSegment";
 
-const ListingReport = (props) => {
-  if (props.fields.length === 0) return <Redirect push to='/' />
+const ListingReport = props => {
+  if (props.fields.length === 0) return <Redirect push to="/" />;
   return (
-    <div style={{ backgroundColor: "white", height: "100%", padding: "45px", color: "#202124" }}>
-      <Header textAlign='center' as='h1'>
+    <div
+      style={{
+        backgroundColor: "white",
+        height: "100%",
+        padding: "45px",
+        color: "#202124"
+      }}
+    >
+      <Header textAlign="center" as="h1">
         <Header.Content>Your StampSafe Stage</Header.Content>
-        <Header.Subheader style={{ padding: "1em" }}>Ensure your privacy, safety and security with StampSafe</Header.Subheader>
+        <Header.Subheader style={{ padding: "1em" }}>
+          Ensure your privacy, safety and security with StampSafe
+        </Header.Subheader>
       </Header>
       <Grid centered columns={2}>
         <Grid.Row stretched>
@@ -24,19 +33,31 @@ const ListingReport = (props) => {
             <StagingDetailsSegment details={props.fields} />
           </Grid.Column>
           <Grid.Column>
-            <DescriptiveLinkSegment header="Criminal Record" subheader="Utilize Accurate Background Checks to run a Federal Criminal Record Search" url="https://www.google.com/" urlText="Start Here" logo={accurateLogo} />
-            <DescriptiveLinkSegment header="Stolen Property" subheader=" This is a non-comprehensive search of various stolen property databases" url="https://www.google.com/" urlText="View Details" logo={safe} />
-           <ActionsSegment />
+            <DescriptiveLinkSegment
+              header="Criminal Record"
+              subheader="Utilize Accurate Background Checks to run a Federal Criminal Record Search"
+              url="https://www.google.com/"
+              urlText="Start Here"
+              logo={accurateLogo}
+            />
+            <DescriptiveLinkSegment
+              header="Stolen Property"
+              subheader=" This is a non-comprehensive search of various stolen property databases"
+              url="https://www.google.com/"
+              urlText="View Details"
+              logo={safe}
+            />
+            <ActionsSegment />
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = state => ({
   fields: state.fields.fields,
   accessKey: state.accessKey.stageAccessKey
-})
+});
 
-export default connect(mapStateToProps)(ListingReport)
+export default connect(mapStateToProps)(ListingReport);
